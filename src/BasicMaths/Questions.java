7be5b5cba269fc09cbb210f2d50e5a7d0ef2dcfd
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2025-04-08 22:16:32 +0530

    Basic Maths - reverseNumber

--- src/BasicMaths/Questions.java
+++ src/BasicMaths/Questions.java
@@ -23,8 +23,19 @@ public class Questions {
         }
         return oddCount;
     }
+
+    public static int reverseNumber (int num) {
+        int rev = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            rev = (rev * 10) + lastDigit;
+            num = num / 10;
+        }
+        return  rev;
+    }
     public static void main(String[] args) {
         System.out.println(countdigitsofanumber(1213234343));
         System.out.println(countOddDigit(1357));
+        System.out.println(reverseNumber(1357));
     }
 }
