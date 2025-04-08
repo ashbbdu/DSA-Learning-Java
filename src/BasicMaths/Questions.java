94a5d0d9c475858fd15f46a9b8720bcb55b5cf53
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2025-04-08 22:05:19 +0530

    Basic Maths - countOddDigit

--- src/BasicMaths/Questions.java
+++ src/BasicMaths/Questions.java
@@ -12,9 +12,19 @@ public class Questions {
     }
 
     public static int countOddDigit (int num) {
-
+        int oddCount = 0;
+        if(num == 0) return 1;
+        while (num > 0) {
+            int rem = num % 10;
+            if(rem % 2 != 0) {
+                oddCount = oddCount + 1;
+            }
+            num = num /10;
+        }
+        return oddCount;
     }
     public static void main(String[] args) {
         System.out.println(countdigitsofanumber(1213234343));
+        System.out.println(countOddDigit(1357));
     }
 }
