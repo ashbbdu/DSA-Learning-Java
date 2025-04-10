32f7d4d7f4e74376f599bd42cf750e4387dccd01
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-10 22:23:42 +0530

    Basic Arrays : revArrBrute

--- src/BasicArrays/Questions.java
+++ src/BasicArrays/Questions.java
@@ -18,10 +18,23 @@ public class Questions {
         }
         return oddCount;
     }
+
+
+    public static void revArrBrute (int [] arr) {
+        for(int i = 0 ; i < arr.length ; i++) {
+           arr[i] = arr[arr.length - 1] - i;
+        }
+    }
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5};
         int size = arr.length;
         System.out.println(sumofarray(arr));
         System.out.println(countOdd(arr , size));
+        revArrBrute(arr);
+        System.out.println(arr[0]);
+
+        for(int i = 0;i < arr.length ;i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
