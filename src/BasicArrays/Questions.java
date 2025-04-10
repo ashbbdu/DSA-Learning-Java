5f96b6567070829218fba256d33ab29a64b503e5
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-10 22:27:44 +0530

    Basic Arrays : revArrOptimal

--- src/BasicArrays/Questions.java
+++ src/BasicArrays/Questions.java
@@ -20,18 +20,36 @@ public class Questions {
     }
 
 
-    public static void revArrBrute (int [] arr) {
-        for(int i = 0 ; i < arr.length ; i++) {
-           arr[i] = arr[arr.length - 1] - i;
+//    public static void revArrBrute (int [] arr) {
+//        for(int i = 0 ; i < arr.length ; i++) {
+//           arr[i] = arr[arr.length - 1] - i;
+//        }
+//    }
+
+    public static void revArrOptimal (int [] arr) {
+//        two pointer approach
+        int start = 0;
+        int end = arr.length - 1;
+        int temp;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
+
+
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5};
         int size = arr.length;
         System.out.println(sumofarray(arr));
         System.out.println(countOdd(arr , size));
-        revArrBrute(arr);
-        System.out.println(arr[0]);
+//        revArrBrute(arr);
+
+        revArrOptimal(arr);
+
 
         for(int i = 0;i < arr.length ;i++) {
             System.out.print(arr[i] + " ");
