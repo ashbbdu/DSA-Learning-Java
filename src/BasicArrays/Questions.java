b10e3fd1f716e10c71d2f0f3527d6c999c6ab2e3
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-10 22:17:38 +0530

    oddcount + sumofarray

---
+++ src/BasicArrays/Questions.java
@@ -0,0 +1,27 @@
+package BasicArrays;
+
+public class Questions {
+    public static int sumofarray (int [] arr) {
+        int sum = 0;
+        for (int i = 0 ; i < arr.length ; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    public static int countOdd(int[] arr, int n) {
+        int oddCount =0;
+        for(int i = 0 ; i < arr.length ; i++) {
+            if(arr[i] % 2 != 0) {
+                oddCount = oddCount + 1;
+            }
+        }
+        return oddCount;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        int size = arr.length;
+        System.out.println(sumofarray(arr));
+        System.out.println(countOdd(arr , size));
+    }
+}
