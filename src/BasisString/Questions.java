42f58ec89654b543cbd9f1af258fef7326e0cd80
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-11 23:49:08 +0530

    reverse an string brute

--- src/BasisString/Questions.java
+++ src/BasisString/Questions.java
@@ -1,10 +1,57 @@
 package BasisString;
 
+import java.util.Stack;
+import java.util.Vector;
+
 public class Questions {
+
+
+    public static void reverseStringBrute (Vector<Character> arr) {
+        Stack<Character> sc = new Stack<>();
+        for(char c : arr ) {
+            sc.push(c);
+        }
+        for (int i = 0; i < arr.size(); ++i) {
+            arr.set(i, sc.pop());
+        }
+        return;
+    }
+
+    public static void reverseStringOptimized (Vector<Character> arr) {
+       int start = 0;
+       int end = arr.size() - 1;
+       char temp;
+       while (start < end) {
+          temp = arr.get(start);
+          arr.set(start , arr.get(end));
+          arr.set(end , temp) ;
+          start++;
+          end--;
+       }
+    }
+
     public static void main(String[] args) {
         String name = "ashish";
         name = name + 'a';
         System.out.println(name);
         System.out.println(name.charAt(1));
+
+        Vector vc = new Vector();
+        vc.add('a');
+        vc.add('b');
+        vc.add('c');
+        vc.add('d');
+//        System.out.println(vc);
+//        System.out.println(vc.size());
+
+//        reverseStringOptimized(vc);
+
+
+        System.out.println(vc);
+
+
+        reverseStringBrute(vc);
+        System.out.println(vc);
+
     }
 }
