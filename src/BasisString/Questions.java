686e70698d5eb4ea2e1b87edff3e0d6bb437bcf3
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-11 23:12:08 +0530

    started strings

---
+++ src/BasisString/Questions.java
@@ -0,0 +1,10 @@
+package BasisString;
+
+public class Questions {
+    public static void main(String[] args) {
+        String name = "ashish";
+        name = name + 'a';
+        System.out.println(name);
+        System.out.println(name.charAt(1));
+    }
+}
