46d568142b70da5e77d73a9bb8ed163d144c4d35
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-13 22:35:23 +0530

    bubble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -5,21 +5,23 @@ public class BubbleSort {
         int[] arr = {7 ,4 ,1 ,5 ,3};
         int n = arr.length;
 // Outer loop controls the number of passes
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    // Swap arr[j] and arr[j + 1]
+        for(int i = 0 ; i < n  - 1  ; i++) {
+            boolean isSwapped = false;
+            for(int j = 0 ; j < n - i - 1 ; j++) {
+                if(arr[j] > arr[j+1]) {
+//                    swap
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSwapped = true;
                 }
             }
-            if (!swapped) {
+            if(!isSwapped) {
                 break;
             }
         }
+
+
         for(int i = 0 ; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
