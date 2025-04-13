e40d58d29e82929c2f5201a37da5d6fd27633e2c
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-13 22:59:08 +0530

    largestElement

--- src/Advance/Arrays/Fundamentals/ArrayFundamentalQuestions.java
+++ src/Advance/Arrays/Fundamentals/ArrayFundamentalQuestions.java
@@ -10,8 +10,19 @@ public class ArrayFundamentalQuestions {
         }
         return largestElement;
     }
+
+    public static int largestElement (int [] nums , int target) {
+        for(int i = 0 ; i < nums.length  ; i++) {
+            if(nums[i] == target) {
+                return i;
+            }
+        }
+//        if element not found
+        return -1;
+    }
     public static void main(String[] args) {
         int arr [] = {2,5,6,9,7};
-        System.out.println(largestElement(arr));
+        System.out. println(largestElement(arr));
+        System.out.println(largestElement(arr , 6));;
     }
 }
