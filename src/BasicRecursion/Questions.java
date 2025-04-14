abd3f7cfd7f44a2c3d80c2c277610fd4062ce2b1
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-14 23:34:02 +0530

    reverseString array

--- src/BasicRecursion/Questions.java
+++ src/BasicRecursion/Questions.java
@@ -1,5 +1,7 @@
 package BasicRecursion;
 
+import java.util.Vector;
+
 public class Questions {
     static  int n  = 0;
     static int cnt = 0;
@@ -84,9 +86,44 @@ public class Questions {
     }
 
 //    Factorial of a given number
-public static int factorialOfANumber (int n) {
+    public static int factorialOfANumber (int n) {
+        if(n <= 1) {
+            return 1;
+        }
+        return n * factorialOfANumber(n -1);
+    }
 
-}
+    public static int sum (int [] arr , int left) {
+        if(left >= arr.length) {
+            return 0;
+        }
+        return arr[left] + sum(arr , left + 1);
+    }
+
+    public static int arrSum (int [] arr , int left) {
+       return sum(arr, 0);
+    }
+
+//    Sum of array elements
+
+
+    public  static void reverse(Vector<Character> s , int left , int right) {
+        if(left >= right) {
+        return;
+        }
+        char temp = s.get(left);
+        s.set(left, s.get(right));
+        s.set(right, temp);
+        reverse(s, left + 1, right - 1);
+
+    }
+
+    public static Vector<Character> reverseString(Vector<Character> s) {
+        int left = 0;
+        int right =  s.size() - 1;
+         reverse(s ,left , right);
+         return s;
+    }
 
     public static void main(String[] args) {
 //        printNameTail();
@@ -104,6 +141,22 @@ public static int factorialOfANumber (int n) {
         System.out.println();
         System.out.println();
         System.out.println(sumoffirstNumbers(4));
+        System.out.println();
+        System.out.println();
+        System.out.println(factorialOfANumber(5));
+
+        int [] arr = {1,2,3};
+        System.out.println(arrSum(arr , 0));
+
+        Vector<Character> s = new Vector<>();
+        s.add('h');
+        s.add('e');
+        s.add('l');
+        s.add('l');
+        s.add('o');
+
+        reverseString(s);
+        System.out.println(s);
     }
 
 }
