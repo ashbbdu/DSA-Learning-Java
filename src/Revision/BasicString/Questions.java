4dd583108abb02f3cf15c5103ed917f123754e19
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-20 23:28:10 +0530

    rotateStringOptimal

--- src/Revision/BasicString/Questions.java
+++ src/Revision/BasicString/Questions.java
@@ -1,5 +1,6 @@
 package Revision.BasicString;
 
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.Vector;
 
@@ -61,6 +62,37 @@ public class Questions {
 
         return  s.substring(zeorIndex+1 , oddIndex+1);
     }
+
+    public static String largestCommonPrefix (String [] s) {
+        Arrays.sort(s);
+        StringBuilder ans = new StringBuilder();
+        String start = s[0];
+        String end = s[s.length - 1];
+        for(int i = 0 ; i < Math.min(start.length() , end.length()) ; i++) {
+            if(start.charAt(i) == end.charAt(i)) {
+              ans =  ans.append(start.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+
+    public static boolean rotateString (String o , String g) {
+        if(o.length() != g.length()) return false;
+        for(int i = 0 ; i < o.length() ; i++) {
+            String right = o.substring(i);
+            String left = o.substring(0 , i);
+            if((right+left).equals(g)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean rotateStringOptimal (String o , String g) {
+        if(o.length() != g.length()) return false;
+        String str = o + o;
+        return str.contains(g);
+    }
     public static void main(String[] args) {
 //        char [] arr = {'h' , 'e' , 'l' , 'l' , 'o'};
 
@@ -82,5 +114,22 @@ public class Questions {
 //        0214638
         System.out.println();
         System.out.println(largestOdd("000214638"));
+
+        String [] brr = {"flowers" , "flow" , "fly", "flight" };
+        System.out.println(largestCommonPrefix(brr));
+//        for(int i = 0 ; i < brr.length ; i++) {
+//            System.out.println(brr[i]);
+//        }
+
+
+
+        String original = "abcde";
+        String goal = "cdeab";
+
+        System.out.println(rotateString(original , goal));
+        System.out.println(original.substring(1));
+        System.out.println(original.substring(0,1));
+
+        System.out.println(rotateStringOptimal("abcde" , "cdeab"));
     }
 }
