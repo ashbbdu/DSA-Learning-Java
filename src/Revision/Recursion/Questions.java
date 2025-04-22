91f5aff9d911ccd629303d8980920b621236c006
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-22 21:11:39 +0530

    sum of first n numbers

--- src/Revision/Recursion/Questions.java
+++ src/Revision/Recursion/Questions.java
@@ -2,6 +2,14 @@ package Revision.Recursion;
 
 public class Questions {
 
+    public static int sumoffirstNNumbers (int n) {
+        if(n == 0) {
+            return 0;
+        }
+        return n + sumoffirstNNumbers(n - 1);
+    }
+
+
     public static long factorial(int n) {
         if(n <= 1) {
             // to handle the zero case also , the case when we have to find the factorial of 0;
@@ -12,5 +20,6 @@ public class Questions {
 
     public static void main(String[] args) {
         System.out.println(factorial(0));
+        System.out.println(sumoffirstNNumbers(5));
     }
 }
