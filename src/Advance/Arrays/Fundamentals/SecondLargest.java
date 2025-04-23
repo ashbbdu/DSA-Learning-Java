87f945051d1b35296fbebde4ec9e47a8dea64a6b
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-23 23:30:36 +0530

    second largest element in an array better

--- src/Advance/Arrays/Fundamentals/SecondLargest.java
+++ src/Advance/Arrays/Fundamentals/SecondLargest.java
@@ -19,13 +19,36 @@ public class SecondLargest {
         return secondLargest;
     }
 
+
+    public static int secondLargestElementBetter(int[] nums) {
+        int n = nums.length;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < n ; i++) {
+            if(largest < nums[i]) {
+                largest = nums[i];
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            if(secondLargest < nums[i] && nums[i] != largest) {
+                secondLargest = nums[i];
+            }
+        }
+//        if second largest is not changed then return -1 handled for [10, 10, 10, 10, 10] this case
+        return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
+    }
+
     public static void main(String[] args) {
-        int [] arr = {3,4,5,1,2,6};
+        int [] arr = {3,4,5,1,2,6,7,9, 10,10};
 //        for(var a : arr) {
 //            System.out.print(a + " ");
 //        }
 
         System.out.println(secondLargestElementBrute(arr));
+
+        System.out.println(secondLargestElementBetter(arr));
 
     }
 }
