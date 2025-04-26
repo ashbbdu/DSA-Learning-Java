f2dc3afcaedd8ae208ec10ab61fd3812e3f603c6
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-26 23:30:40 +0530

    moveZeroes

--- src/Advance/Arrays/Fundamentals/LeftRotateArray.java
+++ src/Advance/Arrays/Fundamentals/LeftRotateArray.java
@@ -11,17 +11,6 @@ public class LeftRotateArray {
             temp[i] = arr[i];
         }
 
-//        for(int i = k ; i < n ; i++) {
-//            arr[i - k] = arr[i];
-//        }
-//
-
-
-// Copy back the k elements at the end
-//        for(int i = 0 ; i < k ; i++ ) {
-//            arr[n - k + i] = temp[i];
-//        }
-
         for(int i = k ; i < n ; i++) {
             arr[i - k] = arr[i];
         }
@@ -32,60 +21,108 @@ public class LeftRotateArray {
 
     }
 
-//    public void rotateArray(int[] nums, int k) {
-//        int n = nums.length;
-//        k = k % n;
-//        int [] temp = new int [k];
-//
-//        for(int i = 0 ; i < k ; i++) {
-//            temp[i] = arr[i];
-//        }
-//
-//        // shifting elements to the start
-//        for(int i = k ; i < n ; i++) {
-//            arr[i - k] = arr[i];
-//        }
-//
-//        // append the temp array at the last
-//        for(int i = 0 ; i < k ; i++) {
-//            arr[n - k + i] = temp[i];
-//        }
-//
-//    }
+    public static void reverse (int [] arr , int start , int end) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
 
+    public static void leftRotateByKOptimal (int [] arr , int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr , 0 , k -1);
+        reverse(arr , k , n - 1);
+        reverse(arr , 0 , n-1);
+    }
 
     public static void rightRotate (int [] arr , int k ) {
         int n = arr.length;
         k = k % n;
-//        {1,2,3,4,5,6,7}
         int [] temp = new int[k];
 
-        for(int i = 0  ; i < temp.length ; i++) {
-            temp[i] = arr[n - k + i];
+
+    }
+
+
+    public static void moveZeroes(int[] nums) {
+//        very brute force approach , hahah
+        int n = nums.length;
+        int count = 0;
+        int nonZeroCount = 0;
+        int c = 0;
+        int d = 0;
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] == 0) {
+                count++;
+            } else {
+                nonZeroCount++;
+            }
         }
 
 
-        for (int i = n - k - 1; i >= 0; i--) {
-            arr[i + k] = arr[i];
+        int [] temp = new int[count];
+        int [] nonZeroTemp = new int[nonZeroCount];
+
+        System.out.println(count + " " + nonZeroCount);
+
+        for(int i = 0 ; i < nums.length; i++ ) {
+            if(nums[i] == 0) {
+                temp[c++] = nums[i];
+             }
         }
 
-        for(int i = 0 ;  i < k ; i++) {
-            arr[i] = temp[i];
+
+        for(int i = 0 ; i < nums.length; i++ ) {
+            if(nums[i] != 0) {
+                nonZeroTemp[d++] = nums[i];
+            }
+        }
+
+
+
+        for(int i = 0 ;i < nonZeroTemp.length ; i++) {
+            nums[i] = nonZeroTemp[i];
+        }
+
+        for(int i = 0 ; i < temp.length ; i++) {
+            nums[nonZeroTemp.length + i] = temp[i];
         }
 
     }
+
+
     public static void main(String[] args) {
-        int arr [] =  {1,2,3,4,5,6,7};
-        int k = 3;
-        leftRotateByK(arr ,k );
-        for(int i = 0 ; i < arr.length ; i++) {
-            System.out.print(arr[i]+ " ");
-        }
-        System.out.println();
-        int brr [] =  {1,2,3,4,5,6,7};
-        rightRotate(brr , 4);
-        for(var a : brr) {
-            System.out.print(a + " ");
+//        int arr [] =  {1,2,3,4,5,6,7};
+//        int k = 4;
+//        leftRotateByK(arr ,k );
+//        for(int i = 0 ; i < arr.length ; i++) {
+//            System.out.print(arr[i]+ " ");
+//        }
+//        System.out.println();
+//        int brr [] =  {1,2,3,4,5,6,7};
+//        rightRotate(brr , 15);
+//        System.out.println();
+////        for(var a : brr) {
+////            System.out.print(a + " ");
+////        }
+//        System.out.println();
+//
+//        int crr[] = {1,2,3,4,5,6,7};
+//                leftRotateByKOptimal(crr , 4);
+//        for(int i = 0 ; i < crr.length ; i++) {
+//            System.out.print(crr[i]+ " ");
+//        }
+
+//        Move all zeros to end of an array
+        int [] drr = {0,1,2,4,0,5,0,1,0,0};
+        moveZeroes(drr);
+        for(var  el : drr) {
+            System.out.print(el + " ");
         }
     }
 }
