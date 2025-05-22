6ecda4f44709ef43f2727a00d49543c824a67059
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-22 23:52:46 +0530

    wip

--- src/TestTest.java
+++ src/TestTest.java
@@ -21,11 +21,36 @@ public class TestTest {
         }
     }
 
+    public static void rotateRightByK (int [] nums , int k) {
+        int n = nums.length;
+        k = k % n;
+        int [] temp = new int [k];
+        for(int i = 0 ; i < k ; i++) {
+            temp[i] = nums[n - k + i];
+        }
+
+//    shift the elements
+        for(int i = n - 1 ; i >= k ; i--) {
+            nums[i] = nums[i - k];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+
+    }
+
     public static void main(String[] args) {
         int [] arr = {1, 2, 3, 4, 5, 6};
         rotateByK(arr , 2);
 
         for(var el : arr) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
+        int [] brr = {1, 2, 3, 4, 5, 6};
+        rotateRightByK(brr , 2);
+
+        for(var el : brr) {
             System.out.print(el + " ");
         }
     }
