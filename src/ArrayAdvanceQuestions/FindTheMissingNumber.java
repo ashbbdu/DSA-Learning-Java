ff8e9a358f392bd94bd8517f223cdaa234fd03b8
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-25 23:45:34 +0530

    findMissingNumberOptimalOne

--- src/ArrayAdvanceQuestions/FindTheMissingNumber.java
+++ src/ArrayAdvanceQuestions/FindTheMissingNumber.java
@@ -18,9 +18,30 @@ public class FindTheMissingNumber {
         return -1;
 
     }
+
+    public static int findMissingNumberBetter (int [] nums) {
+//        betters solution needs hasing concept so will do that after doing hasing
+        return -1;
+    }
+
+    public static int findMissingNumberOptimalOne (int [] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+        int s1 = 0;
+        for(int i = 0 ; i < n ; i++) {
+            s1 = s1 + nums[i];
+        }
+        return sum - s1;
+    }
+
+    public static int findMissingNumberOptimalTwo (int [] nums) {
+
+    }
+
     public static void main(String[] args) {
         int [] arr = {0, 1, 2, 4, 5, 6};
 //        0, 1, 2, 4, 5, 6
         System.out.println( findMissingNumberBrute(arr));
+        System.out.println(findMissingNumberOptimalOne(arr));
     }
 }
