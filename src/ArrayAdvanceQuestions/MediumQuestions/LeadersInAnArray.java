912ba50a1a8a0e499d360eb52f68f002db9d23c0
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-27 23:02:53 +0530

    leadersOptimal

--- src/ArrayAdvanceQuestions/MediumQuestions/LeadersInAnArray.java
+++ src/ArrayAdvanceQuestions/MediumQuestions/LeadersInAnArray.java
@@ -1,6 +1,7 @@
 package ArrayAdvanceQuestions.MediumQuestions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class LeadersInAnArray {
     public static ArrayList<Integer> leadersBrute(int[] nums) {
@@ -19,8 +20,33 @@ public class LeadersInAnArray {
         }
         return ans;
     }
+
+
+    public static ArrayList<Integer> leadersOptimal(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if(nums.length == 0) {
+            return ans;
+        }
+
+        int max = nums[nums.length - 1];
+        ans.add(nums[nums.length - 1]);
+
+        for(int i = nums.length - 2 ; i >= 0 ; i--) {
+            if(nums[i] > max) {
+                max = nums[i];
+                ans.add(nums[i]);
+            }
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
     public static void main(String[] args) {
-        int [] arr = {737,344,561,65,885,18,131,27,335,90,267,336,375,157,548,698,273,342,275,107,561,930,747,383,486,92,242,758,782,594,86,688,978,824,964,497,524,659,114,880,958,280,195,235,341,838,403,573,686,922,388,403,864,722,688,443,714,259,399,93,590,150,652,958,854,289,123,539,782,547,494,132,306,626,761,369,913,480,602};
+        int [] arr = {1, 2, 5, 3, 1, 2};
         System.out.println(leadersBrute(arr));;
+
+        System.out.println(leadersOptimal(arr));
     }
 }
