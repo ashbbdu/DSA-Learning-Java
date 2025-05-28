b29effcd758a240434149bdadd9801846a48c809
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-29 00:27:03 +0530

    twoSumBrute

---
+++ src/ArrayAdvanceQuestions/MediumQuestions/RotateMatrixBy90Deg.java
@@ -0,0 +1,10 @@
+package ArrayAdvanceQuestions.MediumQuestions;
+
+public class RotateMatrixBy90Deg {
+    public static void rotateMatrixBrute(int[][] matrix) {
+
+    }
+    public static void main(String[] args) {
+        int [][] matrix = {{1, 1, 2}, {5, 3, 1}, {5, 3, 5}};
+    }
+}
---
+++ src/ArrayAdvanceQuestions/MediumQuestions/TwoSum.java
@@ -0,0 +1,25 @@
+package ArrayAdvanceQuestions.MediumQuestions;
+
+public class TwoSum {
+    public static int[] twoSumBrute(int[] nums, int target) {
+        int [] arr = new int[2];
+        for(int i = 0 ; i < nums.length ; i++) {
+            for(int j = i+1 ; j < nums.length ; j++) {
+                if(nums[i] + nums[j] == target) {
+                    arr[0] = nums[i];
+                    arr[1] = nums[j];
+                }
+            }
+
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1, 6, 2, 10, 3};
+        int [] brr = twoSumBrute(arr , 7);
+        for(var el : brr) {
+            System.out.println(el);
+        }
+
+    }
+}
