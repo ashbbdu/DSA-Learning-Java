58cf1b8b23b75d3fa172a861fa11629333c552b3
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-02 11:32:41 +0530

    gcdBrute

--- src/NewStart/Basics/Maths/GCD.java
+++ src/NewStart/Basics/Maths/GCD.java
@@ -3,7 +3,7 @@ package NewStart.Basics.Maths;
 public class GCD {
     public static int gcdBrute(int n1, int n2) {
         int gcd = 0;
-        for(int i = 1 ; i <= Math.max(n1 , n2) ; i++) {
+        for(int i = 1 ; i <= Math.min(n1 , n2) ; i++) {
             if(n1 % i == 0 && n2 % i == 0) {
                 gcd = i;
             }
