fdb02340a16d930ad95d12a0c80b7a3771d3f3c3
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-02 22:14:29 +0530

    gcdOptimal

--- src/NewStart/Basics/Maths/GCD.java
+++ src/NewStart/Basics/Maths/GCD.java
@@ -21,8 +21,25 @@ public class GCD {
         }
         return gcd;
     }
+
+    public static int gcdOptimal(int n1, int n2) {
+        while (n1 != 0 && n2 != 0) {
+            if (n1 > n2) {
+//                n1 = n1 - n2;
+                n1 = n1 % n2;
+            } else {
+//                n2 = n2 - n1;
+                n2 = n2 % n1;
+            }
+        }
+
+        if (n1 == 0) return n2;
+        return n1;
+    }
+
     public static void main(String[] args) {
         System.out.println(gcdBrute(9 , 8));
         System.out.println(gcdBetter(4,6));
+        System.out.println(gcdOptimal(35,10));
     }
 }
