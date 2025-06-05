931892fe8c65f77287972e4b3d830cdbf4ba85ff
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-05 12:26:22 +0530

    reverseStringBrute

---
+++ src/NewStart/Basics/Strings/ReverseStringArray.java
@@ -0,0 +1,45 @@
+package NewStart.Basics.Strings;
+
+import java.util.Stack;
+import java.util.Vector;
+
+public class ReverseStringArray {
+    public static void reverseStringBrute (Vector <Character> s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.size()  ; i++) {
+            st.push(s.elementAt(i));
+        }
+//        System.out.println(st);
+        for(int i =0 ; i < s.size() ; i++) {
+            s.set(i , st.pop());
+        }
+    return;
+    }
+
+
+    public static void reverseStringOptimal (Vector <Character> s) {
+        int start = 0;
+        int end = s.size() - 1;
+        while(start < end) {
+            char temp = s.get(start);
+            s.set(start , s.get(end));
+            s.set(end , s.get(temp));
+            start++;
+            end--;
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        Vector<Character> arr = new Vector<>();
+        arr.add('a');
+        arr.add('b');
+        arr.add('c');
+        arr.add('d');
+        arr.add('e');
+        reverseStringBrute(arr);
+        System.out.println(arr);
+        reverseStringOptimal(arr);
+        System.out.println(arr);
+    }
+}
