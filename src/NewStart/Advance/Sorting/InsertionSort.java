30a46d1373865f03832ba9168a568b014f92d88a
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-13 00:23:23 +0530

    InsertionSort

--- src/NewStart/Advance/Sorting/InsertionSort.java
+++ src/NewStart/Advance/Sorting/InsertionSort.java
@@ -2,6 +2,20 @@ package NewStart.Advance.Sorting;
 
 public class InsertionSort {
     public static void main(String[] args) {
+        int [] arr = {7, 4, 1, 5, 3};
 
+        for(int i = 0; i < arr.length ; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j-1]) {
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+
+        for(var el : arr) {
+            System.out.print(el + " ");
+        }
     }
 }
