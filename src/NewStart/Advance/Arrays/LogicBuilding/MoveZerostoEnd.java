52cf141f6ba437958b8cb06aa52f15bdd9efb907
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-15 01:38:35 +0530

    moveZeroesBrute

---
+++ src/NewStart/Advance/Arrays/LogicBuilding/MoveZerostoEnd.java
@@ -0,0 +1,27 @@
+package NewStart.Advance.Arrays.LogicBuilding;
+
+public class MoveZerostoEnd {
+    public static void moveZeroesBrute(int[] nums) {
+        int n = nums.length;
+        int [] temp = new int[n];
+        int count = 0;
+        for (int i = 0 ; i < n ; i++) {
+            if(nums[i] != 0) {
+                temp[count++] = nums[i];
+            }
+        }
+        for(int i = 0 ; i < n ; i++) {
+            nums[i] = temp[i];
+        }
+        for(int i = count ; i < n ; i++) {
+            nums[i] = 0;
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr = {0, 0, 0, 1, 3, -2};
+        moveZeroesBrute(arr);
+        for(var el : arr) {
+            System.out.print(el + " ");
+        }
+    }
+}
