e08fd5ca5edc46c1fe9a0129e5fba94e4ac484bd
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-15 01:47:04 +0530

    moveZeroesOptimal

--- src/NewStart/Advance/Arrays/LogicBuilding/MoveZerostoEnd.java
+++ src/NewStart/Advance/Arrays/LogicBuilding/MoveZerostoEnd.java
@@ -17,10 +17,33 @@ public class MoveZerostoEnd {
             nums[i] = 0;
         }
     }
+    public static void moveZeroesOptimal(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+
+        while(count < n) {
+            nums[count++] = 0;
+        }
+
+
+    }
     public static void main(String[] args) {
         int [] arr = {0, 0, 0, 1, 3, -2};
         moveZeroesBrute(arr);
         for(var el : arr) {
+            System.out.print(el + " ");
+        }
+
+        System.out.println();
+
+        int [] brr = {0, 1, 4, 0, 5, 2};
+        moveZeroesOptimal(brr);
+        for(var el : brr) {
             System.out.print(el + " ");
         }
     }
