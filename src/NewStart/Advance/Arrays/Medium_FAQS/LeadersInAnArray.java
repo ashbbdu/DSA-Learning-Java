8cf91d5879afd93a2a85ca8202b1eab753278b0a
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-20 01:09:08 +0530

    leadersBrute

---
+++ src/NewStart/Advance/Arrays/Medium_FAQS/LeadersInAnArray.java
@@ -0,0 +1,32 @@
+package NewStart.Advance.Arrays.Medium_FAQS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class LeadersInAnArray {
+    public static ArrayList<Integer> leaders(int[] nums) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int max = nums[nums.length - 1];
+        arr.add(nums[nums.length - 1]);
+        for(int i = nums.length - 2 ; i >= 0 ; i--) {
+
+            if(nums[i] > max) {
+                arr.add(nums[i]);
+                max = nums[i];
+            }
+        }
+        Collections.reverse(arr);
+        return arr;
+    }
+    public static void main(String[] args) {
+        int [] arr = {-3, 4, 5, 1, -4, -5};
+//        int[] arr = {
+//                946, 978, 781, 472, 738, 705, 457, 152, 659, 238,
+//                132, 739, 992, 699, 903, 903, 330, 600, 696, 671,
+//                952, 714, 608, 346, 182, 956, 582, 924, 850, 705,
+//                835, 426
+//        };
+        System.out.println(leaders(arr));
+    }
+}
