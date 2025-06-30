73b09888fce616eba2de003dab514bb2bc521aca
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-30 23:38:59 +0530

    rotateArrayOptimal

--- src/AdvanceStart/Arrays/Fundamentals/RotateByK.java
+++ src/AdvanceStart/Arrays/Fundamentals/RotateByK.java
@@ -19,10 +19,31 @@ public class RotateByK {
             nums[i] = temp[i+k-n];
         }
     }
+
+    public static void reverse (int [] nums , int start , int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotateArrayOptimal(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums , 0 , k -1);
+        reverse(nums , k , n -1);
+        reverse(nums , 0 , n -1);
+    }
     public static void main(String[] args) {
         int [] arr = {3, 4, 1, 5, 3, -5};
         rotateArrayBrute(arr , 8);
         System.out.println(Arrays.toString(arr));;
+
+        int [] brr = {3, 4, 1, 5, 3, -5};
+        rotateArrayOptimal(brr , 8);
+        System.out.println(Arrays.toString(brr));;
     }
 }
 
