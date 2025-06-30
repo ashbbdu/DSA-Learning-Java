0526912cb8061eb6d66a0ca1e61f166bc84abe7b
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-30 23:56:43 +0530

    moveZeroesOptima

--- src/AdvanceStart/Arrays/Fundamentals/MoveZerosToEnd.java
+++ src/AdvanceStart/Arrays/Fundamentals/MoveZerosToEnd.java
@@ -23,6 +23,20 @@ public class MoveZerosToEnd {
             nums[i] = 0;
         }
     }
+
+    public static void moveZeroesOptima(int[] nums) {
+        int n = nums.length;
+        int j = 0;
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+
+        while(j < n) {
+            nums[j++] = 0;
+        }
+    }
     public static void main(String[] args) {
         int [] arr = {0, 1, 4, 0, 5, 2};
         moveZeroesBrute(arr);
