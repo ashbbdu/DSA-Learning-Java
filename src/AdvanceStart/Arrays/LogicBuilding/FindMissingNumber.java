9ca2f1ce29d4188426dbc48da954bb30a35c3789
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-02 00:26:28 +0530

    missingNumberOptimal1

--- src/AdvanceStart/Arrays/LogicBuilding/FindMissingNumber.java
+++ src/AdvanceStart/Arrays/LogicBuilding/FindMissingNumber.java
@@ -17,8 +17,21 @@ public class FindMissingNumber {
         }
         return -1;
     }
+    public static int missingNumberOptimal1(int[] nums) {
+        int n = nums.length;
+//         Summation of first N natural numbers
+        int sum1 = n*(n+1)/2;
+        int sum2 = 0;
+        for(var s : nums) {
+            sum2 = sum2 + s;
+        }
+
+        return  sum1  - sum2;
+
+    }
     public static void main(String[] args) {
         int [] arr = {0, 2, 3, 1, 4};
         System.out.println(missingNumberBrute((arr)));
+        System.out.println(missingNumberOptimal1(arr));
     }
 }
