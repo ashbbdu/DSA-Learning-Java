88b034784caff8bb995cd810fb096c3766fc149c
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-03 23:13:57 +0530

    rearrangeArrayOptimal

--- src/AdvanceStart/Arrays/LogicBuilding/RearrangeElementsBySign.java
+++ src/AdvanceStart/Arrays/LogicBuilding/RearrangeElementsBySign.java
@@ -1,9 +1,10 @@
 package AdvanceStart.Arrays.LogicBuilding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-
+//Number of positive and negative numbers are equal.
 public class RearrangeElementsBySign {
-    public static int[] rearrangeArray(int[] nums) {
+    public static int[] rearrangeArrayExtreameBrute(int[] nums) {
         int n = nums.length;
         int a = 0;
         int b = 0;
@@ -18,8 +19,8 @@ public class RearrangeElementsBySign {
                 neg[b++] = nums[i];
             }
         }
-        System.out.println(Arrays.toString(pos));
-        System.out.println(Arrays.toString(neg));
+//        System.out.println(Arrays.toString(pos));
+//        System.out.println(Arrays.toString(neg));
 
         for(int i = 0 ;i < n ; i++) {
             if(i % 2 == 0) {
@@ -30,9 +31,54 @@ public class RearrangeElementsBySign {
         }
         return nums;
     }
+
+    public static int[] rearrangeArrayBrute(int[] nums)  {
+        int n = nums.length;
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] > 0) {
+                pos.add(nums[i]);
+            } else {
+                neg.add(nums[i]);
+            }
+        }
+        for(int i = 0 ; i < n/2 ; i++) {
+            nums[2 * i] = pos.get(i);
+            nums[(2* i )+ 1] = neg.get(i);
+        }
+        return nums;
+    }
+
+    public static int[] rearrangeArrayOptimal(int[] nums)  {
+        int n = nums.length;
+        int posIndex = 0;
+        int negIndex = 1;
+        int [] leaders = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] > 0) {
+               leaders[posIndex] = nums[i];
+               posIndex = posIndex + 2;
+            } else {
+                leaders[negIndex] = nums[i];
+                negIndex = negIndex + 2;
+            }
+        }
+        return leaders;
+    }
+
+
     public static void main(String[] args) {
         int [] arr = {1, -1, -3, -4, 2, 3};
-        int [] brr = rearrangeArray(arr);
+        int [] brr = rearrangeArrayExtreameBrute(arr);
         System.out.println(Arrays.toString(brr));
+
+        int [] crr = {1, -1, -3, -4, 2, 3};
+        int [] drr = rearrangeArrayBrute(crr);
+        System.out.println(Arrays.toString(drr));
+
+        int [] err = {1, -1, -3, -4, 2, 3};
+        int [] frr = rearrangeArrayOptimal(err);
+        System.out.println(Arrays.toString(drr));
     }
 }
