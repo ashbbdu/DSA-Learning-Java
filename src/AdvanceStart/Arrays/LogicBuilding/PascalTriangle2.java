e377f4e2f30978e18b380e620b6b709af7655631
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-06 00:32:11 +0530

    wip

--- src/AdvanceStart/Arrays/LogicBuilding/PascalTriangle2.java
+++ src/AdvanceStart/Arrays/LogicBuilding/PascalTriangle2.java
@@ -1,6 +1,8 @@
 package AdvanceStart.Arrays.LogicBuilding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PascalTriangle2 {
     public static int pascalTriangleI(int r, int c) {
@@ -25,14 +27,26 @@ public class PascalTriangle2 {
         int [] ans = new int[r];
         ans[0] = 1;
         for(int i = 1 ; i < r ; i++) {
-            ans[i] = ans[i - 1] * (r - i) / i;
+            ans[i] = (ans[i - 1] * (r - i)) / i;
         }
         return ans;
     }
+
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> al = new ArrayList<>();
+        al.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            long prev = al.get(i - 1);
+            long val = prev * (rowIndex - i + 1) / i;
+            al.add((int) val);
+        }
+        return al;
+    }
     public static void main(String[] args) {
-        int [] ans = pascalTriangleIIBrute(6);
+        System.out.println(getRow(3));
+        int [] ans = pascalTriangleIIBrute(3);
         System.out.println(Arrays.toString(ans));
-        int [] nums = pascalTriangleIIOptimal(6);
+        int [] nums = pascalTriangleIIOptimal(3);
         System.out.println(Arrays.toString(nums));
     }
 }
