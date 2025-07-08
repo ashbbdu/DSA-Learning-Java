0cc221689e4eefdf181fa48c08c25d72cb05433f
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-08 23:37:11 +0530

    sortZeroOneTwoOptimal

--- src/AdvanceStart/Arrays/LogicBuilding/SortArrayOn0s1s2s.java
+++ src/AdvanceStart/Arrays/LogicBuilding/SortArrayOn0s1s2s.java
@@ -30,9 +30,36 @@ public class SortArrayOn0s1s2s {
         }
 }
 
+    public static void sortZeroOneTwoOptimal(int[] nums) {
+        int n = nums.length;
+        int mid = 0;
+        int low = 0;
+        int high = n-1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }  else if(nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int [] arr = {0, 0, 1, 1, 1};
+        int [] brr = {1, 0, 2, 1, 0};
         sortZeroOneTwoBetter(arr);
         System.out.println(Arrays.toString(arr));
+
+        sortZeroOneTwoOptimal(brr);
+        System.out.println(Arrays.toString(brr));
     }
 }
