5a13326fd87ec182a1135c80a888e31552c2215f
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-10 23:24:20 +0530

    searchRangeBrute

--- src/AdvanceStart/BinarySearch/LogicBuilding/FirstAndLastOccurance.java
+++ src/AdvanceStart/BinarySearch/LogicBuilding/FirstAndLastOccurance.java
@@ -1,12 +1,25 @@
 package AdvanceStart.BinarySearch.LogicBuilding;
 
+import java.util.Arrays;
+
 public class FirstAndLastOccurance {
     public static int[] searchRangeBrute(int[] nums, int target) {
-        int [] range = new int[2];
-        for(int)
+        int first = -1;
+        int last = -1;
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(nums[i] == target) {
+                if(first == -1) {
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        return new int[] {first , last};
     }
     public static void main(String[] args) {
-        int [] nums = {5, 7, 7, 8, 8, 10};
-        int target = 8;
+        int [] nums = {5, 7, 7, 8 , 8, 10};
+        int target = 6;
+        int [] ans = searchRangeBrute(nums , target);
+        System.out.println(Arrays.toString(ans));
     }
 }
