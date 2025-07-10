c80a0924499e18fba129ad2a408239d265b814be
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-10 22:56:24 +0530

    searchInsertBrute

---
+++ src/AdvanceStart/BinarySearch/LogicBuilding/FirstAndLastOccurance.java
@@ -0,0 +1,12 @@
+package AdvanceStart.BinarySearch.LogicBuilding;
+
+public class FirstAndLastOccurance {
+    public static int[] searchRangeBrute(int[] nums, int target) {
+        int [] range = new int[2];
+        for(int)
+    }
+    public static void main(String[] args) {
+        int [] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+    }
+}
--- src/AdvanceStart/BinarySearch/LogicBuilding/Searchinsertposition.java
+++ src/AdvanceStart/BinarySearch/LogicBuilding/Searchinsertposition.java
@@ -10,8 +10,27 @@ public class Searchinsertposition {
         }
         return n;
     }
+
+    public static int searchInsertOptima(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        int mid = start + (end - start) / 2;
+        int ans = n;
+        while (start <= end) {
+            if(nums[mid] >= target) {
+               ans = mid;
+               end = mid - 1;
+            } else {
+                start = start + 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        int [] arr = {1, 3, 5, 6};
-        System.out.println(searchInsertBrute(arr , 7));
+        int [] arr = {1,2,5,7,8,9,11};
+        System.out.println(searchInsertBrute(arr , 10));
+        System.out.println(searchInsertOptima(arr , 10));
     }
 }
