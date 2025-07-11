ccb6b6d644ec014534e9baccb8113b139cf99d23
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-12 00:35:50 +0530

    optimal

--- src/AdvanceStart/BinarySearch/LogicBuilding/SearchInRotatedArray1.java
+++ src/AdvanceStart/BinarySearch/LogicBuilding/SearchInRotatedArray1.java
@@ -20,7 +20,7 @@ public class SearchInRotatedArray1 {
             }
             if(nums[start] <= nums[mid])  { // <= because the array could also be 1,1,1,1,1,1
                 //means the left array is sorted
-                if(nums[start] <= k &&  k <= nums[mid]) { // checking if target is in this range or not
+                if(nums[start] <= k && nums[mid] >= k ) { // checking if target is in this range or not
 //                    if element is present in this rannge
                     end = mid - 1;
                 } else {
@@ -29,7 +29,7 @@ public class SearchInRotatedArray1 {
             }
             else {
 //                nums[start] > nums[end] means the right array is sorted
-                if((nums[mid] <= k && k <= nums[end])) {
+                if((nums[mid] <= k &&  nums[end] >= k)) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
@@ -41,10 +41,43 @@ public class SearchInRotatedArray1 {
 
         return -1;
     }
+
+    public static boolean searchOptimala(int[] nums, int k) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        while(start <= end) {
+            if(nums[mid] == k) {
+                return true;
+            }
+            if(nums[start] <= nums[mid])  { // <= because the array could also be 1,1,1,1,1,1
+                //means the left array is sorted
+                if(nums[start] <= k && nums[mid] >= k ) { // checking if target is in this range or not
+//                    if element is present in this rannge
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            else {
+//                nums[start] > nums[end] means the right array is sorted
+                if((nums[mid] <= k &&  nums[end] >= k)) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+            mid = start + (end - start) / 2;
+        }
+
+        return false;
+    }
     public static void main(String[] args) {
-        int [] nums = {4, 5, 6, 7, 0, 1, 2};
-        int k = 0;
+        int [] nums = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int k = 10;
         System.out.println(searchBrute(nums , k));
         System.out.println(searchOptimal(nums , k));
+        System.out.println(searchOptimala(nums , k));
     }
 }
---
+++ src/AdvanceStart/BinarySearch/LogicBuilding/SearchInRotatedArray2.java
@@ -0,0 +1,7 @@
+package AdvanceStart.BinarySearch.LogicBuilding;
+
+public class SearchInRotatedArray2 {
+    public static void main(String[] args) {
+
+    }
+}
