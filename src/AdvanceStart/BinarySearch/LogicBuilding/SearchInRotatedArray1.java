a3cbe1b493c5f97eaa4a0f9fc102dcfce5c45363
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-11 23:11:48 +0530

    searchOptimal

--- src/AdvanceStart/BinarySearch/LogicBuilding/SearchInRotatedArray1.java
+++ src/AdvanceStart/BinarySearch/LogicBuilding/SearchInRotatedArray1.java
@@ -9,9 +9,30 @@ public class SearchInRotatedArray1 {
         }
         return -1;
     }
+
+    public static int searchOptimal(int[] nums, int k) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        int ans = -1;
+
+        while(start <= end) {
+            if(nums[mid] == k) {
+                ans = mid;
+            } else if (nums[mid] < k) {
+                start = mid + 1;
+            } else {
+
+                end = mid - 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int [] nums = {4, 5, 6, 7, 0, 1, 2};
-        int k = 5;
+        int k = 0;
         System.out.println(searchBrute(nums , k));
+        System.out.println(searchOptimal(nums , k));
     }
 }
