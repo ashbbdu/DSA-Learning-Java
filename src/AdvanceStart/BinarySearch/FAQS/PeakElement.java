8b794aee9cbeca9526b6bac44b75b0f8cac95a6b
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-20 00:06:07 +0530

    brute updated

--- src/AdvanceStart/BinarySearch/FAQS/PeakElement.java
+++ src/AdvanceStart/BinarySearch/FAQS/PeakElement.java
@@ -3,9 +3,22 @@ package AdvanceStart.BinarySearch.FAQS;
 public class PeakElement {
     public static int findPeakElementBrute(int[] arr) {
         int n = arr.length;
-        for(int i  = 0 ; i < n ; i++) {
 
-            if((i == 0 || arr[i - 1] < arr[i]) && (i == arr.length - 1  || arr[i] > arr[i+1]) ) {
+//        for(int i  = 0 ; i < n ; i++) {
+//
+//            if((i == 0 || arr[i - 1] < arr[i]) && (i == arr.length - 1  || arr[i] > arr[i+1]) ) {
+//                return i;
+//            }
+//        }
+        if (n == 1) return 0;
+        if(arr[0] > arr[1]) {
+            return 0;
+        }
+        if(arr[n - 1] > arr[n - 2]) {
+            return n - 1;
+        }
+        for(int i  = 1 ; i < n - 1; i++) {
+            if(arr[i - 1] < arr[i] && arr[i] > arr[i+1]) {
                 return i;
             }
         }
