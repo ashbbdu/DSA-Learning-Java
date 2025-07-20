7d02f9cf96e541366fb4c96700a238adbdd4b75d
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-20 22:15:09 +0530

    wip

--- src/AdvanceStart/BinarySearch/FAQS/PeakElement.java
+++ src/AdvanceStart/BinarySearch/FAQS/PeakElement.java
@@ -24,9 +24,34 @@ public class PeakElement {
         }
         return -1;
     }
+
+    public static int findPeakElementOptimal(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return 0;
+        if(arr[0] > arr[1]) {
+            return 0;
+        }
+        if(arr[n - 1] > arr[n - 2]) {
+            return n - 1;
+        }
+        int start = 1;
+        int end = n -2;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-//        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
-        int [] arr = {1, 2, 4, 8, 10};
+        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
+//        int [] arr = {1, 2, 4, 8, 10};
         System.out.println(findPeakElementBrute(arr));
+        System.out.println(findPeakElementOptimal(arr));
     }
 }
