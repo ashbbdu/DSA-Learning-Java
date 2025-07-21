6ca1031c1d92a3a56794331b474aece2d16a370c
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-21 22:32:36 +0530

    floorSqrtOptimal

--- src/AdvanceStart/BinarySearch/OnAnswers/FindSquareRoot.java
+++ src/AdvanceStart/BinarySearch/OnAnswers/FindSquareRoot.java
@@ -1,14 +1,23 @@
 package AdvanceStart.BinarySearch.OnAnswers;
 
 public class FindSquareRoot {
+    public static long floorSqrtBrute(long n) {
+        for(long i = 1 ; i<=n ; i++) {
+            if(i * i > n) {
+                return i - 1;
+            }
+        }
+        return -1;
+    }
     public static long floorSqrtOptimal(long n) {
         long start = 1;
         long end = n;
-        long mid = start + (end - start)/2;
 
         long ans = 1;
         if(n == 0) return 0;
+        if(n == 1) return 1;
         while(start <= end) {
+            long mid = start + (end - start)/2;
             if(mid * mid <= n) {
                 ans = mid;
                 start = mid + 1;
@@ -16,12 +25,13 @@ public class FindSquareRoot {
               end = mid - 1;
 
             }
-            mid = start + (end - start)/2;
+
         }
         return ans;
     }
     public static void main(String[] args) {
-        int n =50;
+        int n = 380980991;
         System.out.println(floorSqrtOptimal(n));
+        System.out.println(floorSqrtBrute(n));
     }
 }
