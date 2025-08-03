b1a93e3d8c5e9c9015af9dadbc4c3d6e8af6eb08
ashbbdu <ashishsrivastava7007@gmail.com>
2025-08-03 18:48:48 +0530

    roseGardenOptimal

--- src/AdvanceStart/BinarySearch/OnAnswers/MBouquets.java
+++ src/AdvanceStart/BinarySearch/OnAnswers/MBouquets.java
@@ -32,11 +32,35 @@ public class MBouquets {
         }
         return -1;
     }
+
+
+    public static int roseGardenOptimal(int n, int[] nums, int k, int m) {
+
+        long val = (long) m * k;
+        if (val > n) return -1;
+        int start = Integer.MAX_VALUE, end = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            start = Math.min(start, nums[i]);
+            end = Math.max(end, nums[i]);
+        }
+       int ans = -1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(calculateBouquets(nums , k , mid) >= m) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        int [] nums = {7, 7, 7, 7, 13, 11, 12, 7};
-        int k = 3;
-        int m = 2;
+        int [] nums = {1, 10, 3, 10, 2};
+        int k = 1;
+        int m = 3;
         System.out.println(roseGardenBrute(nums.length , nums , k , m));
+        System.out.println(roseGardenOptimal(nums.length , nums , k , m));
 //        System.out.println(4/3);
     }
 }
