d3fb87e862f5ee6b50cda0865fa038ea26fc76c8
ashbbdu <ashishsrivastava7007@gmail.com>
2025-08-16 16:35:44 +0530

    hashing questions

--- src/AdvanceStart/HashingQuestions/CountFrequencies.java
+++ src/AdvanceStart/HashingQuestions/CountFrequencies.java
@@ -4,6 +4,5 @@ public class CountFrequencies {
     public static void main(String[] args) {
         System.out.println("Count Frequencies");
         System.out.println("Will solve hashing questions for 2-3 days");
-        System.out.println("Hashing");
     }
 }
