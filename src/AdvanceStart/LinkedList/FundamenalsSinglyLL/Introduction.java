5654514b03bfb399989489ab80cbdbccd32f952f
ashbbdu <ashishsrivastava7007@gmail.com>
2025-09-18 23:31:51 +0530

    linkedlist

--- src/AdvanceStart/LinkedList/FundamenalsSinglyLL/Introduction.java
+++ src/AdvanceStart/LinkedList/FundamenalsSinglyLL/Introduction.java
@@ -3,10 +3,16 @@ package AdvanceStart.LinkedList.FundamenalsSinglyLL;
 
 
 public class Introduction {
-    class Node {
-
+   static class Node {
+        String name ;
+        String email ;
+        Node (String _name , String _email) {
+            this.name = _name;
+            this.email = _email;
+        }
     }
     public static void main(String[] args) {
-
+        Node n1 = new  Node("ashish" , "ashish@gmail.com");
+        System.out.println(n1.name);
     }
 }
